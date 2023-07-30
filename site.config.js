ac679054a3189ec853cb3ba3b9ed3aa02c256e4f
Mari <45552388+thehue@users.noreply.github.com>
2023-07-30 16:27:33 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,21 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    name: "hue",
+    image: "https://notion-avatar.vercel.app/api/img/eyJmYWNlIjowLCJub3NlIjoxMCwibW91dGgiOjExLCJleWVzIjowLCJleWVicm93cyI6OSwiZ2xhc3NlcyI6MCwiaGFpciI6MjEsImFjY2Vzc29yaWVzIjoxMywiZGV0YWlscyI6MywiYmVhcmQiOjAsImZsaXAiOjAsImNvbG9yIjoicmdiYSgyNTUsIDAsIDAsIDApIiwic2hhcGUiOiJzcXVhcmUifQ==", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "nhhy32@gmail.com",
+    linkedin: "",
+    github: "thehue",
     instagram: "",
   },
   projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "hue-log",
+    description: "welcome to hue-log!",
   },
 
   // CONFIG configration (required)
@@ -47,9 +43,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "thehue/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
